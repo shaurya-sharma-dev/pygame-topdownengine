e12131c585f307fe1e15cf19220202378857e163
Shaurya Sharma <sshauryaofficial@gmail.com>
2026-06-17 19:28:23 -0400

    Refactor basic usage example

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,5 +1,17 @@
 import topdownengine as tdg
 
-game = tdg.Game(900, 650)
-game.game_object_group.add(tdg.GameObject())
+# Define an instance of the Game class
+game = tdg.Game(
+    screen_width=900, 
+    screen_height=650, 
+    window_title="pygame-topdownengine Basic Usage Example"
+)
+
+# Define a game object
+game_object = tdg.GameObject()
+
+# Add it to the game object group
+game.game_object_group.add(game_object)
+
+# Run the game
 game.run()
