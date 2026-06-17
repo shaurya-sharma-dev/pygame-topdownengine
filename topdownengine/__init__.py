ee47e9fe69794436424af49ce7f8c5df71a1052f
Shaurya Sharma <sshauryaofficial@gmail.com>
2026-06-17 13:45:31 -0400

    Add starter files + Git test

---
+++ topdownengine/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2026 Shaurya Sharma
+# SPDX-License-Identifier: MIT
+
+"""pygame-topdownengine is a game engine built on top of the pygame-ce
+framework. It allows for the quick creation of 2.5D top-down games."""
+
+from .game import Game
---
+++ topdownengine/game.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026 Shaurya Sharma
+# SPDX-License-Identifier: MIT
+
+import pygame as pg
+
+class Game:
+    def __init__(self) -> None:
+        pass
