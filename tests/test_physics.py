add99fbf8272ecc72318f27ee3ebe5eff97f334f
Shaurya Sharma <sshauryaofficial@gmail.com>
2026-06-18 14:27:08 -0400

    Refactor test_physics and added some code for 2D movements tests for later.

--- tests/test_physics.py
+++ tests/test_physics.py
@@ -3,13 +3,14 @@
 
 import topdownengine as tde
 from topdownengine.mob_controller import BaseMobController
-from topdownengine.asset_paths import ASSETS_DIR
 import pytest
+import pygame as pg
 
 @pytest.fixture
 def mob():
     return tde.Mob(controller=BaseMobController(), headless=True)
 
+# Jump Tests
 def test_can_jump_while_grounded(mob):
     mob.elevation = mob.z = 0
     mob.jump()
@@ -20,3 +21,18 @@ def test_cannot_jump_while_airborne(mob):
     mob.z = 10
     mob.jump()
     assert mob.z_vel != mob.jump_vel
+
+# 2D Movement Tests
+MOVEMENT_TEST_ARGS = [
+    pg.Vector2(1, 0), # Right
+    pg.Vector2(0, 1), # Down
+    pg.Vector2(-1, 0), # Left
+    pg.Vector2(0, -1), # Up
+    pg.Vector2(1, 1), # Down-Right
+    pg.Vector2(1, -1), # Up-Right
+    pg.Vector2(-1, 1), # Down-Left
+    pg.Vector2(-1, -1) # Up-Left
+]
+@pytest.mark.parametrize("dir", MOVEMENT_TEST_ARGS)
+def test_movement(mob, dir):
+    pass
