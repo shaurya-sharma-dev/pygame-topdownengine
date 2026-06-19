378bb250448cdcfe9d936679f6a5b157302eb706
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-06-18 21:16:48 -0400

    Add EnvObj to basic usage example and import it in __init__.py. Also, add option to turn on subpixel rendering in basic usage example.
    Signed-off-by: Shaurya Sharma 65916881+shaurya-sharma-dev@users.noreply.github.com

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,6 +1,7 @@
 import topdownengine as tde
 from topdownengine.mobile_obj.controller import KeyboardInputController
 from topdownengine.asset_paths import ASSETS_DIR
+import pygame as pg
 
 # Define an instance of the Game class
 game = tde.Game(
@@ -9,8 +10,8 @@ game = tde.Game(
     window_title="pygame-topdownengine Basic Usage Example"
 )
 
-# Define a mobile_obj
-game_object = tde.MobileObj(
+# Define a MobileObj
+mobile_obj = tde.MobileObj(
     controller=KeyboardInputController(), 
     animation_paths={
         'idle': ASSETS_DIR / 'example-player' / 'idle.png',
@@ -20,11 +21,19 @@ game_object = tde.MobileObj(
     directional_anims=True
 )
 
-# Add it to the game object group
-game.game_object_group.add(game_object)
+# Define an env_obj
+env_obj = tde.EnvObject(colliders=[pg.Rect(0, 0, 16, 16)])
+env_obj.position = pg.Vector2(100, 100)
+
+# Add them both to the game object group
+game.game_object_group.add(mobile_obj)
+game.game_object_group.add(env_obj)
 
 # Rescale GameObjects to have a SCALE of 3 (this makes them more visible)
 tde.GameObject.set_scale(3, game)
+
+# You can add subpixel rendering by uncommenting the below line of code
+# tde.GameObject.SUBPIXEL = True
 
 # Run the game
 game.run()
