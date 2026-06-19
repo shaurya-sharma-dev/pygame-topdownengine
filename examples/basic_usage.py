e6311b7c3e31425ad3ffd385cd614271b8105700
Shaurya Sharma <sshauryaofficial@gmail.com>
2026-06-18 22:22:31 -0400

    Add collisions and debug draw to basic usage example.

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,6 +1,7 @@
 import topdownengine as tde
 from topdownengine.mobile_obj.controller import KeyboardInputController
 from topdownengine.asset_paths import ASSETS_DIR
+from topdownengine.math import scale_rect
 import pygame as pg
 
 # Define an instance of the Game class
@@ -22,8 +23,9 @@ mobile_obj = tde.MobileObj(
 )
 
 # Define an env_obj
-env_obj = tde.EnvObject(colliders=[pg.Rect(0, 0, 16, 16)])
+env_obj = tde.EnvObject(frame_size=(32, 32), colliders=[pg.Rect(0, 0, 32, 32)])
 env_obj.position = pg.Vector2(100, 100)
+env_obj.obj_shadow = '32x16'
 
 # Add them both to the game object group
 game.game_object_group.add(mobile_obj)
@@ -34,6 +36,25 @@ tde.GameObject.set_scale(3, game)
 
 # You can add subpixel rendering by uncommenting the below line of code
 # tde.GameObject.SUBPIXEL = True
+
+original_draw = game.render
+def new_render():
+    original_draw()
+    pg.draw.rect(
+        game.screen, 
+        (0, 0, 255), 
+        scale_rect(mobile_obj.hitboxes[0], mobile_obj.SCALE),
+        1
+    )
+    pg.draw.rect(
+        game.screen, 
+        (0, 0, 255), 
+        scale_rect(env_obj.hitboxes[0], env_obj.SCALE),
+        1
+    )
+    pg.display.flip()
+
+game.render = new_render
 
 # Run the game
 game.run()
