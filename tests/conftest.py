6bccbe4a4bc3bd5a857bf59359d7697400ca25e9
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-06-18 21:01:10 -0400

    Migrate fixtures to brand new conftest file. Also, add 2d movement test to test_physics.
    Signed-off-by: Shaurya Sharma 65916881+shaurya-sharma-dev@users.noreply.github.com

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2026 Shaurya Sharma
+# SPDX-License-Identifier: MIT
+
+import pygame as pg
+import pytest
+import topdownengine as tde
+from topdownengine.mob_controller import KeyboardInputController
+from topdownengine.asset_paths import ASSETS_DIR
+from topdownengine.controls import MoreKeysPressed
+
+# Fixtures
+@pytest.fixture
+def game():
+    game = tde.Game(1, 1)
+    print('Initializing game instance.')
+    yield game
+    pg.quit()
+
+@pytest.fixture
+def mob():
+    return tde.Mob(
+        controller=KeyboardInputController(), 
+        animation_paths={
+            'idle': ASSETS_DIR / 'example-player' / 'idle.png',
+            'walk': ASSETS_DIR / 'example-player' / 'walk.png'
+        },
+        frame_size=(16, 16),
+        directional_anims=True
+    )
+
+# This code "monkey patches" pygame-ce to replace get_pressed with a custom
+# function that allows us to add/remove fake keys to the stream by 
+# adding/removing them from the FAKED_KEYS set.
+FAKED_KEYS = set()
+original_get_pressed = pg.key.get_pressed
+
+def fake_get_pressed():
+    return MoreKeysPressed(original_get_pressed(), FAKED_KEYS)
+
+pg.key.get_pressed = fake_get_pressed
