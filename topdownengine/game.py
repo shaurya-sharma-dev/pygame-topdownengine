4b1f4561c8725720b36f444df79ccc46301b158d
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-06-23 21:29:58 -0400

    Fix Game docstring.
    Signed-off-by: Shaurya Sharma 65916881+shaurya-sharma-dev@users.noreply.github.com

--- topdownengine/game.py
+++ topdownengine/game.py
@@ -5,23 +5,23 @@ import pygame as pg
 from .math import scale_rect
 
 class Game:
-    VALID_EXTRA_FEATURES = {'resize',}
-
     """Acts as the central core of the game and manages the core loop and gamestate.
     
     Attributes:
-    - screen (pygame.Surface): The primary display Surface.
-    - is_running (bool): Boolean flag to control execution.
-    - clock (pygame.time.Clock): Controls framerate and handles deltatime.
-    - fps (int): Integer that controls how much FPS the Game should have
-    - game_object_group (pygame.sprite.Group): Stores all GameObjects.
-    - game_speed_percentage (float): The speed percentage for execution, ranging from `0` to `1`.
-    - debug (bool): If `True`, debug rendering will be enabled.
-    - target_ratio (float): Target aspect ratio for resizing.
-    - target_scale (int): The target scale for the original window size.
-    - og_width (int): Original window width.
-    - extra_features (list[str]): List of extra features to add at runtime. You MUST set it during instantiation.
+        screen (pygame.Surface): The primary display Surface.
+        is_running (bool): Boolean flag to control execution.
+        clock (pygame.time.Clock): Controls framerate and handles deltatime.
+        fps (int): Integer that controls how much FPS the Game should have
+        game_object_group (pygame.sprite.Group): Stores all GameObjects.
+        game_speed_percentage (float): The speed percentage for execution, ranging from `0` to `1`.
+        debug (bool): If `True`, debug rendering will be enabled.
+        target_ratio (float): Target aspect ratio for resizing.
+        target_scale (int): The target scale for the original window size.
+        og_width (int): Original window width.
+        extra_features (list[str]): List of extra features to add at runtime. You MUST set it during instantiation.
     """
+
+    VALID_EXTRA_FEATURES = {'resize',}
 
     def __init__(
         self, 
