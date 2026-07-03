275c69de8030bc2e082294b3d5766b3bb7ae9097
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-07-03 14:34:51 -0400

    Remove UIScene and implement UIContainer support in the BaseScene class. This allows the GameplayScene to also use UIContainers.
    Signed-off-by: Shaurya Sharma 65916881+shaurya-sharma-dev@users.noreply.github.com

--- topdownengine/scenes.py
+++ topdownengine/scenes.py
@@ -5,21 +5,35 @@ from .ui import UIContainer
 class BaseScene:
     def __init__(self, game):
         self.game = game
+        self._ui_containers = []
+
+    @property
+    def ui_containers(self) -> list[UIContainer]:
+        return self._ui_containers
+    
+    @ui_containers.setter
+    def ui_containers(self, new_containers: list[UIContainer]):
+        self._ui_containers = new_containers
 
     def handle_event(self, event: pg.Event):
-        pass
+        for container in self.ui_containers:
+            container.handle_event(event)
     
     def update(self, dt: float):
-        pass
+        for container in self.ui_containers:
+            container.update(dt)
 
     def render(self):
-        pass
+        for container in self.ui_containers:
+            container.render(self.game.screen)
     
 class GameplayScene(BaseScene):
     def update(self, dt):
+        super().update(dt)
         self.game.game_object_group.update(dt, self.game)
 
     def render(self):
+        super().render()
         for game_obj in sorted(self.game.game_object_group.sprites(), key=lambda g: g.draw_index):
             self.game.screen.blit(game_obj.image, game_obj.rect.move(-self.game.camera.position * game_obj.SCALE))
         
@@ -34,17 +48,3 @@ class GameplayScene(BaseScene):
                         scale_rect(hitbox.move(-self.game.camera.position), game_obj.SCALE),
                         1
                     )
-
-class UIScene(BaseScene):
-    def __init__(self, game):
-        super().__init__(game)
-        self.container = UIContainer()
-
-    def handle_event(self, event: pg.Event):
-        self.container.handle_event(event)
-    
-    def update(self, dt: float):
-        self.container.update(dt)
-
-    def render(self):
-        self.container.render(self.game.screen)
