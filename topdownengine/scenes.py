41a1075865cec3a892694de0619f63f2a0b17f17
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-07-02 22:01:36 -0400

    Add UI scene.
    Signed-off-by: Shaurya Sharma 65916881+shaurya-sharma-dev@users.noreply.github.com

--- topdownengine/scenes.py
+++ topdownengine/scenes.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from .math import scale_rect
+from .ui import UIContainer
 
 class BaseScene:
     def __init__(self, game):
@@ -33,3 +34,17 @@ class GameplayScene(BaseScene):
                         scale_rect(hitbox.move(-self.game.camera.position), game_obj.SCALE),
                         1
                     )
+
+class UIScene(BaseScene):
+    def __init__(self, game):
+        super().__init__(game)
+        self.container = UIContainer()
+
+    def handle_event(self, event: pg.Event):
+        self.container.handle_event(event)
+    
+    def update(self, dt: float):
+        self.container.update(dt)
+
+    def render(self):
+        self.container.render(self.game.screen)
