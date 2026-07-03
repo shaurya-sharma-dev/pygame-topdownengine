59f97b6463603996433be3c9378f36716eda570e
Shaurya Sharma <sshauryaofficial@gmail.com>
2026-07-02 22:01:20 -0400

    Add ui module with UIContainer, BaseUIElement, and Button classes.

---
+++ topdownengine/ui.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+import pygame as pg
+from .game import Game
+from collections.abc import Callable
+
+class UIContainer:
+    def __init__(self):
+        self.elements = set()
+    
+    def add_ui_element(self, element: BaseUIElement) -> None:
+        self.elements.add(element)
+        element.containers.add(self)
+
+    def remove_ui_element(self, element: BaseUIElement) -> None:
+        self.elements.remove(element)
+        element.containers.remove(self)
+
+    def handle_event(self, event: pg.Event) -> None:
+        for e in self.elements:
+            e.handle_event(event)
+    
+    def update(self, dt: float) -> None:
+        for e in self.elements:
+            e.update(dt)
+
+    def render(self, surface: pg.Surface) -> None:
+        for e in self.elements:
+            surface.blit(e.image, e.rect)
+
+class BaseUIElement:
+    def __init__(self, game: Game, position: pg.typing.Point, align: str="center", image: pg.Surface=None):
+        self.game = game
+        self.containers = set()
+        self._image = image
+        if self._image is None:
+            self._image = pg.Surface((1,1), pg.SRCALPHA)
+        self.rect = self._image.get_rect(**{align: position})
+
+    @property
+    def image(self) -> pg.Surface:
+        return self._image
+
+    def add_container(self, container: UIContainer) -> None:
+        self.containers.add(container)
+        container.elements.add(self)
+
+    def remove_container(self, container: UIContainer) -> None:
+        self.containers.remove(container)
+        container.elements.remove(self)
+
+    def handle_event(self, event: pg.Event) -> None:
+        pass
+    
+    def update(self, dt: float) -> None:
+        pass
+
+class Button(BaseUIElement):
+    def __init__(self, game: Game, position: pg.typing.Point, align: str="center", image: pg.Surface=None, on_click: Callable[[], None]=None, hover_highlight_strength: int=100):
+        super().__init__(game, position, align, image)
+        self.on_click = on_click
+        self.hover_highlight_strength = hover_highlight_strength
+
+    @property
+    def image(self) -> pg.Surface:
+        return (self._image.copy().fill((self.hover_highlight_strength, self.hover_highlight_strength, self.hover_highlight_strength, 0), special_flags=pg.BLEND_RGBA_ADD) or self._image) if self.is_mouse_over() else self._image
+
+    def is_mouse_over(self) -> bool:
+        return self.rect.collidepoint(pg.mouse.get_pos())
+    
+    def handle_event(self, event: pg.Event) -> None:
+        if event.type == pg.MOUSEBUTTONUP and self.is_mouse_over() and self.on_click is not None:
+            self.on_click()
