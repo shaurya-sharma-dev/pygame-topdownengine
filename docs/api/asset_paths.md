9472c002fc7e3c7103bcba7c1736b3c5adbb05c4
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-07-07 11:38:57 -0400

    Fix asset_paths module API reference.
    Signed-off-by: Shaurya Sharma 65916881+shaurya-sharma-dev@users.noreply.github.com

--- docs/api/asset_paths.md
+++ docs/api/asset_paths.md
@@ -1,3 +1,5 @@
 # topdownengine.asset_paths Reference
 
 ::: topdownengine.asset_paths
+    options:
+      show_attribute_values: false
--- topdownengine/asset_paths.py
+++ topdownengine/asset_paths.py
@@ -3,5 +3,8 @@
 
 from pathlib import Path
 
-PACKAGE_ROOT = Path(__file__).resolve().parent
-ASSETS_DIR = PACKAGE_ROOT / "assets"
+PACKAGE_ROOT: Path = Path(__file__).resolve().parent
+"The folder that represents the root directory of the package."
+
+ASSETS_DIR: Path = PACKAGE_ROOT / "assets"
+"The folder that represents the assets directory of the package."
