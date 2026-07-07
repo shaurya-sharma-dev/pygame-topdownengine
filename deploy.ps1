30949402796f44f1de2c728b9db2a1c9805e5af5
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-07-07 16:41:12 -0400

    Update documentation

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,5 @@
 Write-Host "Starting build + deployment of docs..." -ForegroundColor Cyan
 zensical build --clean
 ghp-import -n -p -f site
+Write-Host "Deployment finished. The documentation should be up at https://shaurya-sharma-dev.github.io/pygame-topdownengine/ soon." -ForegroundColor Green
+Write-Host "Visit https://github.com/shaurya-sharma-dev/pygame-topdownengine/actions/workflows/pages/pages-build-deployment for GitHub pages deployment runs."
