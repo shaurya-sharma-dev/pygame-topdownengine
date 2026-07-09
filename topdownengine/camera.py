93ba7e330f00ae74db651314d6fef9908635127c
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-07-09 16:07:52 -0400

    Refactor Camera class.
    Signed-off-by: Shaurya Sharma 65916881+shaurya-sharma-dev@users.noreply.github.com

--- topdownengine/camera.py
+++ topdownengine/camera.py
@@ -24,26 +24,35 @@ class Camera:
         "Get the position of the camera with screenshake."
         return self.real_position + (self.screenshake_offset if self.screenshake["duration"] > 0 else pg.Vector2())
 
-    def update(self, dt: float):
-        """Update the Camera.
-
-        Args:
-            dt (float): The deltatime.
-        """
+    def _update_screenshake(self, dt: float) -> None:
         if self.screenshake["duration"] > 0:
             self.screenshake_offset = pg.Vector2(
                 random.uniform(-self.screenshake["intensity"], self.screenshake["intensity"]),
                 random.uniform(-self.screenshake["intensity"], self.screenshake["intensity"]),
             )
         self.screenshake["duration"] = max(0, self.screenshake["duration"] - (dt/1000)) # dt is in milliseconds
-        
-        if self.focus_game_object:
-            screen = pg.display.get_surface()
-            self.real_position = pg.Vector2(self.focus_game_object.position) - pg.Vector2(
-                screen.width / GameObject.SCALE / 2, 
-                screen.height / GameObject.SCALE / 2
-            )
-        
-        # Bounds
+
+    def _track_game_object(self) -> None:
+        screen = pg.display.get_surface()
+        self.real_position = pg.Vector2(self.focus_game_object.position) - pg.Vector2(
+            screen.width / GameObject.SCALE / 2, 
+            screen.height / GameObject.SCALE / 2
+        )
+
+    def _handle_bounds(self) -> None:
         self.real_position.x = max(0, self.position.x)
         self.real_position.y = max(0, self.position.y)
+
+    def update(self, dt: float) -> None:
+        """Update the Camera.
+
+        Args:
+            dt (float): The deltatime.
+        """        
+        self._update_screenshake(dt)
+
+        if self.focus_game_object:
+            self._track_game_object()
+        
+        # Bounds
+        self._handle_bounds()
