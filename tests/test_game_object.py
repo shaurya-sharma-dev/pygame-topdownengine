8e7fe7fede0cea307a1291b746f2931bcfa6570f
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-07-14 18:00:05 -0400

    Add velocity deadzone tests to GameObject test suite.
    Signed-off-by: Shaurya Sharma 65916881+shaurya-sharma-dev@users.noreply.github.com

--- tests/test_game_object.py
+++ tests/test_game_object.py
@@ -3,6 +3,7 @@
 
 import topdownengine as tde
 import pytest
+import pygame as pg
 
 # GameObjectGroup Tests
 # We add the game fixture to initialize the display.
@@ -42,3 +43,21 @@ class TestGameObjectGroup:
         group.remove(game_object)
 
         assert game_object not in group.game_objects
+
+def test_clears_velocity_if_in_velocity_deadzone_range(game: tde.Game):
+    game_object = tde.GameObject()
+    game.game_object_group.add(game_object)
+
+    game_object.velocity = pg.Vector2(game_object.VELOCITY_DEADZONE, 0)
+    game.update(1000 / game.fps)
+
+    assert game_object.velocity == pg.Vector2()
+
+def test_does_not_clear_velocity_if_not_in_velocity_deadzone_range(game: tde.Game):
+    game_object = tde.GameObject()
+    game.game_object_group.add(game_object)
+
+    game_object.velocity = pg.Vector2(game_object.VELOCITY_DEADZONE + 0.1, 0)
+    game.update(1000 / game.fps)
+
+    assert game_object.velocity != pg.Vector2()
