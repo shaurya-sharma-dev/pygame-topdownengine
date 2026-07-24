7d740177da82563eda991662b5b515c88a8da1e4
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-07-24 14:21:20 -0400

    feat(tests): add screenshake test for Camera.
    Signed-off-by: Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -4,8 +4,9 @@
 import topdownengine as tde
 import pytest
 import pygame as pg
+import math
 
-@pytest.mark.parametrize("position", [
+POSITIONS = [
     pg.Vector2(5, 8),
     pg.Vector2(4, 7),
     pg.Vector2(5, 9),
@@ -15,7 +16,9 @@ import pygame as pg
     pg.Vector2(5, 7),
     pg.Vector2(4, 6),
     pg.Vector2(3, 2),
-])
+]
+
+@pytest.mark.parametrize("position", POSITIONS)
 def test_base_camera_instantly_snaps_to_position_if_focus_game_object_is_set(game: tde.Game, position: pg.Vector2):
     game_object = tde.GameObject()
     game.game_object_group.add(game_object)
@@ -28,3 +31,14 @@ def test_base_camera_instantly_snaps_to_position_if_focus_game_object_is_set(gam
         game.screen.width / tde.GameObject.SCALE / 2, 
         game.screen.height / tde.GameObject.SCALE / 2
     ) == game.camera.real_position
+
+@pytest.mark.parametrize("position", POSITIONS)
+@pytest.mark.parametrize("intensity", range(1, 4)) # Use intensities of 1, 2, and 3 because the lowest distance from (0, 0) is ~3.6.
+def test_distance_of_position_from_real_position_within_intensity_times_sqrt_2_if_screenshake_set_on_base_camera(game: tde.Game, position: pg.Vector2, intensity: int):
+    game.camera.real_position = position
+    game.camera.screenshake = {"duration": 99, "intensity": intensity}
+    game.update(1000 / game.fps)
+
+    # Test that the distance between screenshake position and real position is within
+    # intensity multiplied by the square root of 2.
+    assert game.camera.real_position.distance_to(game.camera.position) <= intensity * math.sqrt(2)
