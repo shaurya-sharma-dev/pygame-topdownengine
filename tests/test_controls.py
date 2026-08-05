9ea024e45e64128cb472f96b1869da45a274f4e2
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-08-04 20:33:02 -0400

    tests: add controls module tests.

---
+++ tests/test_controls.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2026 Shaurya Sharma
+# SPDX-License-Identifier: MIT
+
+import topdownengine as tde
+from topdownengine.controls import KeyboardInputManager, NoKeysPressed
+import pygame as pg
+
+class TestKeyboardInputManager:
+    def test_deserializing_creates_same_keybinds_dict_if_using_serialized_version_of_original_keybinds_dict(self, game: tde.Game):
+        manager = KeyboardInputManager()
+        manager.keybinds["Custom Keybind"] = pg.K_AC_BACK
+        serialized_manager_keybinds = manager.serialize()
+
+        new_manager = KeyboardInputManager()
+        new_manager.deserialize(serialized_manager_keybinds)
+
+        assert manager.keybinds == new_manager.keybinds
+
+def test_getting_index_value_is_equal_to_false_if_using_no_keys_pressed_instance():
+    assert not NoKeysPressed()[0]
