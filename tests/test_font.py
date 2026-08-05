78b5d3cb567008d94e0148b223c8ff1d2668eee3
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-08-05 19:02:32 -0300

    tests: add test for Font.get_max_size_for_text_in_rect.

--- tests/test_font.py
+++ tests/test_font.py
@@ -10,3 +10,12 @@ def test_draw_text_method_raises_value_error_if_given_invalid_align_value(game: 
     surf = pg.Surface((1,1))
     with pytest.raises(ValueError):
         font.draw_text("test", 0, 0, 1, surf, "black", "invalid alignment")
+
+# TODO: Add more paramatrized values.
+@pytest.mark.parametrize("text,target_rect,expected_size", [
+    ["test", pg.Rect(0, 0, 100, 50), 50],
+    ["test", pg.Rect(0, 0, 100, 1), 2]
+])
+def test_get_max_size_for_text_in_rect_method(game: tde.Game, text: str, target_rect: pg.Rect, expected_size: int):
+    font = tde.Font("Arial")
+    assert font.get_max_size_for_text_in_rect(text, target_rect) == expected_size
